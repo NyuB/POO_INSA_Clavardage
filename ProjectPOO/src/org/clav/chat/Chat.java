3c6d485ef0486ac7d2f5841e28f6b4bab30f7eb9
M. Gouvine-Birrer Denis <gouvine-@insa-08114>
2019-12-06 14:34:26 +0100

    1er proto chat

--- ProjectPOO/src/org/clav/chat/Chat.java
+++ ProjectPOO/src/org/clav/chat/Chat.java
@@ -1,16 +1,38 @@
 package org.clav.chat;
 
+import org.clav.network.NetworkManager;
 import org.clav.user.User;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Chat {
-	private HashMap<String, User> members;
+	
+	private User mainUser ;
+	private NetworkManager networkManager;
+	
+	//Le mainUser n'est pas dans le members !!
+	private ArrayList<User> members;
 	private History history;
 	
-	public Chat(HashMap<String, User> members) {		
+	public Chat(ArrayList<User> members, NetworkManager network, User user) {
+		this.mainUser = user ;
+		this.networkManager = network ;
 		this.members = members ;
 		this.history = new History() ;
+		
+		for(User u : members) {
+			networkManager.initiateConnectionTCP(u.getIdentifier());
+		}
+		
+	}
+	
+	public void sendMessage(String message) {
+		for(User u : members) {
+			networkManager.TCP_IP_send(u.getIdentifier(), message) ;
+		}
+		//TODO gerer le cas où l'envoi foire
+		history.insertMessage(new Message(mainUser.getIdentifier() ,message)) ;
 	}
 	
 	public void loadHistory() {
---
+++ ProjectPOO/src/org/clav/chat/TestChat.java
@@ -0,0 +1,66 @@
+package org.clav.chat;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import org.clav.Agent;
+import org.clav.network.NetworkManager;
+import org.clav.network.ProtocolInit;
+import org.clav.network.protocolsimpl.tcp.TCPListenerProtocol;
+import org.clav.network.protocolsimpl.udp.ActivitySignalProtocol;
+import org.clav.network.protocolsimpl.udp.ActivitySignalProtocolInit;
+import org.clav.network.protocolsimpl.udp.UDPListenerProtocol;
+import org.clav.user.User;
+import org.clav.user.UserManager;
+
+public class TestChat {
+	
+	public static void main(String[] args) {
+		
+		//setup
+		Scanner in  = new Scanner(System.in);
+		System.out.println("Enter user name");
+		String name = in.nextLine();
+		Agent agent = new Agent();
+		User mainUser = new User(name,name);
+		UserManager userManager = new UserManager(agent,mainUser);
+		NetworkManager networkManager = null;
+		String line;
+		try {
+			InetAddress localAddr = InetAddress.getByName("0.0.0.0");
+			System.out.println("Enter broadcast address");
+			line = in.nextLine();
+			InetAddress broadcastAddr = InetAddress.getByName(line);
+			networkManager = new NetworkManager(localAddr,broadcastAddr);
+			networkManager.setRelatedAgent(agent);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		agent.setNetworkManager(networkManager);
+		agent.setUserManager(userManager);
+		
+		//who's there ?
+		networkManager.executeProtocol(new UDPListenerProtocol(new ProtocolInit(networkManager)));
+		networkManager.executeProtocol(new TCPListenerProtocol(new ProtocolInit(networkManager)));
+		
+		//chat
+		ArrayList<User> members = new ArrayList<User>() ;
+		HashMap<String, User> users = userManager.getActiveUsers() ;
+		for (User u : users.values()) {
+			members.add(u) ;
+		}
+		Chat chat = new Chat(members, networkManager,userManager.getMainUser()) ;
+		
+		System.out.println("Enter command");
+		while(!(line=in.nextLine()).equals("END")){
+		
+			if(line.equals("SEND")){
+				String message = in.nextLine();
+				chat.sendMessage(message) ;
+			}
+		}
+		
+	}
+}

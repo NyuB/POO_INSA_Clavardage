1f963e5969d1380985c7f8df55f5b9000d53be2b
M. Gouvine-Birrer Denis <gouvine-@insa-10223>
2019-12-18 12:06:56 +0100

    Creation db OK + Insertion quasi Ok

--- ProjectPOO/src/org/clav/config/Installer.java
+++ ProjectPOO/src/org/clav/config/Installer.java
@@ -7,6 +7,10 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
@@ -15,6 +19,22 @@ public class Installer {
 	
 	public void install(){
 
+	}
+	
+	public void createDB() {
+		try {
+			Class.forName("org.hsqldb.jdbc.JDBCDriver");
+			Connection con = DriverManager.getConnection("jdbc:hsqldb:file:db/mydb", "SA", "") ;
+			Statement stm = con.createStatement() ;
+			String query = "Create Table Chats(codeChat VARCHAR(32) ,userid VARCHAR(25), date Date, text VARCHAR(1024), Primary Key (codeChat, date) ) "  ;
+			stm.executeUpdate(query) ;
+			con.close() ;
+		} 
+		catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public void createDefaultConfig(short mask) {
--- ProjectPOO/src/org/clav/database/TestLocalDB.java
+++ ProjectPOO/src/org/clav/database/TestLocalDB.java
@@ -2,16 +2,24 @@ package org.clav.database;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.clav.chat.Message;
+import org.clav.config.Installer;
+import org.clav.user.User;
 
 public class TestLocalDB {
 	
 	public static void main(String[] args) {
-		
 		LocalStorage db = new LocalStorage() ;
-		ResultSet rs = db.request("select * from test") ;
+		ArrayList<User> members = new ArrayList<User>() ; 
+		members.add(new User("bob", "bob")) ;
+		members.add(new User("michel", "michel")) ;
+		db.storeMessage(new Message("id", "hash", "Je Suis Un Test")) ;
+		ResultSet rs = db.request("select * from Chats") ;
 		try {
 			while(rs.next()) {
-				System.out.println(rs.getString(1)) ;
+				System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getDate(3) + " " + rs.getString(4)) ;
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block

a6a997ed4c1f414844ee8edf2680303669e9b234
NyuB <decaeste@etud.insa-toulouse.fr>
2019-12-20 09:18:56 +0100

    Basic tests for each manager

--- ProjectPOO/test/clavtests/unit/AgentUnitTest.java
+++ ProjectPOO/test/clavtests/unit/AgentUnitTest.java
@@ -1,10 +1,35 @@
 package clavtests.unit;
 
+import org.clav.Agent;
+import org.clav.chat.ChatManager;
+import org.clav.network.NetworkManager;
+import org.clav.user.User;
+import org.clav.user.UserManager;
 import org.junit.Test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class AgentUnitTest {
 	@Test
-	public void startAndStop(){
+	public void construct(){
+		Agent agent = new Agent();
+	}
+
+	@Test
+	public void setStartAndStop(){
+		Agent agent = new Agent();
+		agent.setChatManager(new ChatManager());
+
+		try {
+			InetAddress localhost = InetAddress.getByName("localhost");
+			agent.setNetworkManager(new NetworkManager(localhost,localhost));
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		agent.setUserManager(new UserManager(new User("Main","Main")));
+		agent.start();
+		agent.stop();
 
 	}
 }
--- ProjectPOO/test/clavtests/unit/NetworkManagerUnitTest.java
+++ ProjectPOO/test/clavtests/unit/NetworkManagerUnitTest.java
@@ -1,10 +1,61 @@
 package clavtests.unit;
 
+import org.clav.Agent;
+import org.clav.chat.ChatManager;
+import org.clav.network.NetworkManager;
+import org.clav.user.User;
+import org.clav.user.UserManager;
+
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class NetworkManagerUnitTest {
+
 	@Test
-	public void startUDPOnly(){
+	public void constructorTest() throws UnknownHostException {
+		NetworkManager networkManager;
+		InetAddress localhost = InetAddress.getByName("localhost");
+		networkManager = new NetworkManager(localhost, localhost);
+
 
 	}
+
+	@Test
+	public void aloneStartAndStopSignal() throws InterruptedException, UnknownHostException {
+		int initialCount = Thread.activeCount();
+		NetworkManager networkManager;
+		InetAddress localhost = InetAddress.getByName("localhost");
+		networkManager = new NetworkManager(localhost, localhost);
+		networkManager.startUDPSignal();
+		Thread.sleep(1000);
+		//Assert.assertEquals("UDP launch should have failed due to appHandler absence",initialCount,Thread.activeCount());
+		networkManager.stopActivitySignal();
+		networkManager.startUDPSignal();
+		Thread.sleep(1000);
+		//Assert.assertEquals("UDP launch should have failed due to appHandler absence",initialCount,Thread.activeCount());
+		networkManager.stopActivitySignal();
+	}
+
+	@Test
+	public void withAgentStartAndStopSignal() throws InterruptedException, UnknownHostException {
+		int initialCount = Thread.activeCount();
+		Agent agent = new Agent();
+		agent.setChatManager(new ChatManager());
+		InetAddress localhost = InetAddress.getByName("localhost");
+		NetworkManager networkManager =  new NetworkManager(localhost, localhost);
+		agent.setNetworkManager(networkManager);
+		networkManager.setAppHandler(agent);
+		agent.setUserManager(new UserManager(new User("Main", "Main")));
+		networkManager.startUDPSignal();
+		Thread.sleep(1000);
+		Assert.assertEquals("UDP thread launched and should be running",initialCount+1,Thread.activeCount());
+		networkManager.stopActivitySignal();
+		Thread.sleep(1000);
+		Assert.assertEquals("UDP thread stopped",initialCount,Thread.activeCount());
+	}
+
+
 }
